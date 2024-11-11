d91c85a0c59d3efe80ddfc27f1f82966b85b16bd
Jake <marchmanjake@gmail.com>
2024-11-10 19:35:27 -0500

    project finished!

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+var multer = require('multer')
 
 var app = express();
 
 app.use(cors());
+app.use(express.json())
+const upload = multer({ dest: "uploads/" });
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single("upfile"), function (req, res){
+  console.log("Endpoint hit")
+  console.log(req.file)
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+});
 
 
 const port = process.env.PORT || 3000;
